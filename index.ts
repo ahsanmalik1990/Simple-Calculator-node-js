9860b1b84eca4a38805745011153e652f6ed3836
ahsanmalik1990 <malikje26@gmail.com>
2024-12-19 21:40:39 +0500

    Add files via upload
    Simple Calculator node js project

---
+++ index.ts
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First number", types: "number", name: "firstnumber" },
+  
+  {
+    message: "select One of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["+", "-", "*", "/"],
+  },
+
+  { message: "Enter Second number", types: "number", name: "secondnumber" }
+
+
+]);
+
+// conditional statement
+if(answer.operator === "+"){
+console.log(Number(answer.firstnumber) +Number (answer.secondnumber));
+console.log(Number(answer.firstnumber) + answer.secondnumber);
+
+} else if (answer.operator === "-") {
+  console.log(answer.firstnumber - answer.secondnumber);
+} else if (answer.operator === "*") {
+  console.log(answer.firstnumber * answer.secondnumber);
+} else if (answer.operator === "/") {
+  console.log(answer.firstnumber / answer.secondnumber);
+} else {
+  console.log("Please select valid operator");
+}
+
+
